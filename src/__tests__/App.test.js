4e9e25cd209679e515f0934b36f8cd30756d211d
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2020-05-27 20:40:13 +0300

    upd

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -21,7 +21,9 @@ test("Простой тест", () => {
     };
     app.getData(mock_state);
     //console.log(app.state);
-    expect(app.state.report_data.factor).toBe(2.308);
+    //expect(app.state.report_data.factor).toBe(2.308);
+    expect(app.state.report_data).toMatchSnapshot();
+   
 });
 
 test("Простой тест c моментами", () => {
@@ -44,5 +46,38 @@ test("Простой тест c моментами", () => {
   };
   app.getData(mock_state);
   //console.log(app.state);
-  expect(app.state.report_data.factor).toBe(2.841);
+  //expect(app.state.report_data.factor).toBe(2.841);
+  expect(app.state.report_data).toMatchSnapshot();
+});
+
+test("Армирование", () => {
+  let component;
+  act(() => {
+      component = create(
+        <App></App>
+      );
+  });
+  var app = component.getInstance();
+  var mock_state = {
+    input_n_load: 40,
+    input_mx_load: 5,
+    input_a_column_size: 500,
+    input_b_column_size: 200,
+    input_t_slab_size: 200,
+    input_a_slab_size: 30,
+    concrete_grade: "b30",
+    shear_bars_diameter: "8",
+    shear_bars_grade: "a240c",
+    shear_bars_row_number: 2,
+    input_shear_bars_spacing_to_prev: [0, 50, 100],
+    shear_reinforcement: true,
+    shear_bars_number: {
+      X: [0, 5, 7],
+      Y: [0, 4, 6],
+    },
+  };
+  app.getData(mock_state);
+  //console.log(app.state);
+  //expect(app.state.report_data.factor).toBe(0.962);
+  expect(app.state.report_data).toMatchSnapshot();
 });

abc10e21177849304de16a17638f246d129c43c1
Aleksey Malakhov <hexel@tut.by>
2020-05-29 21:57:38 +0300

    upd

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -110,7 +110,7 @@ test("Колонна рядом с 1 краем плиты без армиров
   expect(app.state.report_data).toMatchSnapshot();
 });
 
-test.only("Колонна рядом с 1 краем плиты c армированием", () => {
+test("Колонна рядом с 1 краем плиты c армированием", () => {
   let component;
   act(() => {
       component = create(
@@ -144,5 +144,74 @@ test.only("Колонна рядом с 1 краем плиты c армиров
   app.getData(mock_state);
   //console.log(app.state);
   //expect(app.state.report_data.factor).toBe(2.111);
+  expect(app.state.report_data).toMatchSnapshot();
+});
+
+test("Колонна на углу плиты без армирования", () => {
+  let component;
+  act(() => {
+      component = create(
+        <App></App>
+      );
+  });
+  var app = component.getInstance();
+  var mock_state = {
+    input_n_load: 50,
+    input_mx_load: 5,
+    input_my_load: 3,
+    input_a_column_size: 400,
+    input_b_column_size: 200,
+    input_t_slab_size: 200,
+    input_a_slab_size: 30,
+    concrete_grade: "b20",
+    slab_edge: true,
+    edge_top: true,
+    edge_left: true,
+    input_edge_left_dist: 0,
+    input_edge_top_dist: 0,
+  };
+
+  app.getData(mock_state);
+  //console.log(app.state);
+  //expect(app.state.report_data.factor).toBe(6.938);
+  expect(app.state.report_data).toMatchSnapshot();
+});
+
+test.only("Колонна на углу плиты c армированием", () => {
+  let component;
+  act(() => {
+      component = create(
+        <App></App>
+      );
+  });
+  var app = component.getInstance();
+  var mock_state = {
+    input_n_load: 50,
+    input_mx_load: 5,
+    input_my_load: 3,
+    input_a_column_size: 400,
+    input_b_column_size: 200,
+    input_t_slab_size: 200,
+    input_a_slab_size: 30,
+    concrete_grade: "b20",
+    slab_edge: true,
+    edge_top: true,
+    edge_left: true,
+    input_edge_left_dist: 0,
+    input_edge_top_dist: 0,
+    shear_bars_diameter: "8",
+    shear_bars_grade: "a240c",
+    shear_bars_row_number: 2,
+    input_shear_bars_spacing_to_prev: [0, 50, 100],
+    shear_reinforcement: true,
+    shear_bars_number: {
+      X: [0, 5, 7],
+      Y: [0, 4, 6],
+    },
+  };
+
+  app.getData(mock_state);
+  //console.log(app.state);
+  //expect(app.state.report_data.factor).toBe(3.649);
   expect(app.state.report_data).toMatchSnapshot();
 });

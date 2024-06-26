35279cf09f6411395ae3901f09f4b92e6319ea13
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-06-26 17:19:36 +0300

    add comment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     plugins: [react()],
     test: {
         environment: "jsdom",
+        // globals: true means we may not import  "escribe, expect, it" in every test - vitest provides them
         globals: true,
         setupFiles: "./tests/setup.js",
     },

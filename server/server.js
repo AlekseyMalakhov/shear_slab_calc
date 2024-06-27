acefd624f91a2934651e53183a72b93a00942ff1
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-06-27 10:17:59 +0300

    server works

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 import express from "express";
-const app = express();
+import path from "path";
+import { fileURLToPath } from "url";
 
-const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
 
 app.use(express.static(__dirname + "/dist"));
 
@@ -9,6 +13,7 @@ app.get("*", function (req, res) {
     res.sendFile(__dirname + "/dist/index.html");
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
 });
